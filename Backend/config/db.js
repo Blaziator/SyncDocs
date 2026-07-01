3678c03d6544273605198a9e87bf1a40dc5c7f9b
Bhavya Kumawat <bhavya.k1005@gmail.com>
2026-07-01 21:05:01 +0530

    feat: add server entry point with DB connection

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async()=>{
     try{
         await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connection established with MongoDB")
+        console.log("Connection established with MongoDB");
     }catch(err){
         console.error(`Failed to connect to the DB, Error: ${err}`);
         process.exit(1);
