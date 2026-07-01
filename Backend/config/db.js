77ae49ffa410ea43c95ac1454d335be590817015
Bhavya Kumawat <bhavya.k1005@gmail.com>
2026-07-01 20:50:33 +0530

    feat: add MongoDB connection setup

---
+++ Backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connection established with MongoDB")
+    }catch(err){
+        console.error(`Failed to connect to the DB, Error: ${err}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
