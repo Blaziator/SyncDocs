c7df3942d63283a03fac7ca674eead295ad4341d
Bhavya Kumawat <bhavya.k1005@gmail.com>
2026-07-18 04:01:44 +0530

    refactor: connect server to Socket.IO

--- Backend/server.js
+++ Backend/server.js
@@ -3,13 +3,11 @@ import {createServer} from "http";
 import { Server } from "socket.io";
 import connectDB from "./config/db.js";
 import app from "./app.js";
+import {setupCollaboration} from "./socket/collaboration.js";
 
 const PORT = process.env.PORT || 8080;
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-    cors: {origin: process.env.CLIENT_URL, credentials: true}
-});
-
+setupCollaboration(httpServer);
 
 connectDB().then(()=>{
     httpServer.listen(PORT, () => {
