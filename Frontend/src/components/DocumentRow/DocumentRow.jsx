7429dc7d014e197a72f7a05cc1f61205f2cf6109
Bhavya Kumawat <bhavya.k1005@gmail.com>
2026-07-21 00:44:37 +0530

    feat: add share button and sharing functionality to dashboard

--- Frontend/src/components/DocumentRow/DocumentRow.jsx
+++ Frontend/src/components/DocumentRow/DocumentRow.jsx
@@ -1,7 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import { formatDistanceToNow } from "date-fns";
 import {useAuth} from "../../context/AuthContext.jsx";
-import { Trash2, SquarePen, FileText } from "lucide-react";
+import ShareModal from "../ShareModal/ShareModal.jsx";
+import { useState } from "react";
+import { Trash2, SquarePen, FileText, Share2 } from "lucide-react";
 import styles from "./DocumentRow.module.css";
 
 export default function DocumentRow({ doc, onDelete, onRename }) {
@@ -10,10 +12,17 @@ export default function DocumentRow({ doc, onDelete, onRename }) {
     const {user} = useAuth();
     const isShared = doc.owner && doc.owner._id !== user.id;
 
+    const [isShareOpen, setIsShareOpen] = useState(false);
+
     const relativeTime = formatDistanceToNow(new Date(doc.updatedAt), { addSuffix: true });
 
+    const handleRowClick = () => {
+        if (isShareOpen) return;
+        navigate(`/doc/${doc._id}`);
+    };
+
   return (
-    <div className={styles.row} onClick={()=> navigate(`/doc/${doc._id}`)}>
+    <div className={styles.row} onClick={handleRowClick}>
 
         <div className={styles.info}>
             <span className={styles.title}> <FileText size={22} color="blue"/> {doc.title}</span>
@@ -38,7 +47,17 @@ export default function DocumentRow({ doc, onDelete, onRename }) {
                     onClick={(e) => { e.stopPropagation(); onRename(doc); }}
                 >
                     <SquarePen size={18}/>
-                </button>
+            </button>
+
+            {!isShared && 
+
+              <button
+                  className={styles.shareBtn}
+                  onClick={(e) => {e.stopPropagation(); setIsShareOpen(true)}}
+              >
+                  <Share2 size={18} />
+              </button>
+            }
 
             <button 
                 className={styles.deleteBtn} 
@@ -51,6 +70,11 @@ export default function DocumentRow({ doc, onDelete, onRename }) {
             </button>
         </div>
 
+        <ShareModal
+                isOpen={isShareOpen}
+                docId={doc._id}
+                onClose={() => setIsShareOpen(false)}
+        />
 
     </div>
   );
