6a1f728a06df1ded09efe37ae9427eb3c0ec936e
Bhavya Kumawat <bhavya.k1005@gmail.com>
2026-07-21 20:46:56 +0530

    feat: add AppError class, asyncWrapper, and centralized error handling middleware

---
+++ Backend/middleware/errorHandler.js
@@ -0,0 +1,8 @@
+export const errorHandler = (err, req, res, next) => {
+    console.error(err);
+
+    const statusCode = err.statusCode || 500;
+    const message = err.statusCode? err.message: "Something went wrong. Please try again.";
+
+    res.status(statusCode).json({ message });
+};
